0833cc27841c22ea7f6a8af5b132eba12b7ba06e
Burak-Yigit <68034322+Burak-Yigit@users.noreply.github.com>
2020-07-11 18:30:28 +0300

    Create hello.c
    problem set 1

---
+++ problems/2020/x/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+
+    {
+        string name = get_string("Enter your name here, please.\n");
+
+    printf("Hello and welcome aboard %s\n", name);
+}
